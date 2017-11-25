404c029b9867e1b7713a44e598576f233cd6ab2b
Chaim Eliyah <chaimeliyah@gmail.com>
2017-11-24 20:16:51 -0800

    adding virtualbox vagrant vagrant-manager

---
+++ cask/README.md
@@ -0,0 +1,2 @@
+
+Note that for `install-virtualbox.sh` you have to open System Preferences and give Oracle permission to install software.
---
+++ cask/install-vagrant.sh
@@ -0,0 +1,5 @@
+brew cask install vagrant
+
+# a GUI manager for Vagrant boxes on the machine;
+# see http://vagrantmanager.com/
+brew cask install vagrant-manager
